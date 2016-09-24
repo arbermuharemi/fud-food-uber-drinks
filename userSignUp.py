5b3c3d7f275bd756840c829ebba57c0241ee985a
Rishabh Mahajani <rmahajani3@gatech.edu>
2016-09-24 08:32:52 -0400

    Worked on adding user data to database

--- userSignUp.py
+++ userSignUp.py
@@ -18,7 +18,21 @@ def checkUserName(username):
     for row in results:
         for entry in row:
             if (username == entry):
-                isValid = False;
+                isValid = False
     return isValid
 
+def addUserToDatabase(username, password, firstname, lastname, height, weight, drinks, dietaryRestrictions, food, amountOwed):
+    username_insert = '\'' + username + '\''
+    password_insert = '\'' + password + '\''
+    firstname_insert = '\'' + firstname + '\''
+    lastname_insert = '\'' + lastname + '\''
+    drinks_insert = '\'' + drinks + '\''
+    dietaryRestrictions_insert = '\'' + dietaryRestrictions + '\''
+    food_insert = '\'' + food + '\''
+    height_insert = '\'' + height + '\''
+    sql = """insert into users (UserName, Password, FirstName, LastName, Height, Weight, Drinks, DietaryRestrictions, Food, AmountOwed)
+    values(""" + username_insert + ',' + password_insert + ',' +  firstname_insert + ',' + lastname_insert + ',' + height + ',' + weight + ',' + drinks_insert + ',' + dietaryRestrictions_insert + ',' + food_insert + ',' + amountOwed + """)"""
+    cursor.execute(sql)
+
+addUserToDatabase('jbutler', 'frontend', 'Jacob', 'Butler', '5 feet 11 inches', 190, 'Ginger Ale', 'Not Applicable', 'Sandwiches', 0)
 print (checkUserName("amuharemi"))
