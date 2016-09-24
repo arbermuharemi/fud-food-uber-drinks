92d5caf8270288c73e9439101b2a9b975e8e6b7e
Jacob Butler <jbutler72@gatech.edu>
2016-09-24 10:29:23 -0400

    Welcome Page looks good!

--- testDeltaApi.py
+++
@@ -1,6 +0,0 @@
-import urllib2
-
-def getFlightInfo():
-	return urllib2.urlopen("https://demo30-test.apigee.net/v1/hack/status?flightNumber=1500&flightOriginDate=2016-09-25&apikey=ZINxBqol4GEAB9L1T25ZcFyG9vmapoLW").read()
-
-print (getFlightInfo())
