68bbcc33ed8c77ba2231fec31249b0482d5dbb16
Rishabh Mahajani <rmahajani3@gatech.edu>
2016-09-24 19:14:28 -0400

    Fixing any merge conflicts

--- python/userSignUp.py
+++ python/userSignUp.py
@@ -1,4 +1,6 @@
 import MySQLdb
+from flask import Flask, redirect, url_for, request, render_template
+app = Flask(__name__)
 
 host = "localhost"
 server_user = "root"
@@ -8,31 +10,56 @@ database = "user_accounts"
 db = MySQLdb.connect(host, server_user, server_password, database)
 
 cursor = db.cursor()
+firstname = ""
+lastname = ""
 
-def checkUserName(username):
+@app.route('/getNames', methods = ['GET', 'POST'])
+def getNames():
+    firstname = request.form['firstName']
+    lastname = request.form['lastName']
+
+@app.route('/checkUserName', methods=['GET', 'POST'])
+def checkUserName():
     sql = """select UserName from users;"""
     cursor.execute(sql)
     results = cursor.fetchall()
     isValid = True
+    username = request.form['userField']
+    password = request.form['passField']
 
     for row in results:
         for entry in row:
             if (username == entry):
                 isValid = False
-    return isValid
+    if (isValid):
+        return redirect(url_for('addUserToDatabase1'), username = username, password = password, firstname = firstname, lastname = lastname)
+    else:
+        return redirect(url_for('incorrectUser'))
 
-def addUserToDatabase(username, password, firstname, lastname, height, weight, drinks, dietaryRestrictions, food, amountOwed):
+@app.route('/incorrectUser')
+def incorrectUser():
+    return "This username is taken. Please sign up with another username."
+
+@app.route('/addUserToDatabase1')
+def addUserToDatabase1(username, password, firstname, lastname):
     username_insert = '\'' + username + '\''
     password_insert = '\'' + password + '\''
     firstname_insert = '\'' + firstname + '\''
     lastname_insert = '\'' + lastname + '\''
+    columnsStatement = 'insert into users (UserName, Password, FirstName, LastName)'
+    sql = columnsStatement + 'values(%s, %s, %s, %s)' %(username_insert, password_insert, firstname_insert, lastname_insert)
+    cursor.execute(sql)
+    db.commit()
+
+def addUserToDatabase2(height, weight, drinks, dietaryRestrictions, food, amountOwed):
     drinks_insert = '\'' + drinks + '\''
     dietaryRestrictions_insert = '\'' + dietaryRestrictions + '\''
     food_insert = '\'' + food + '\''
     height_insert = '\'' + height + '\''
-    columnsStatement = 'insert into users (UserName, Password, FirstName, LastName, Height, Weight, Drinks, DietaryRestrictions, Food, AmountOwed)'
-    sql = columnsStatement + 'values(%s, %s, %s, %s, %s, %d, %s, %s, %s, %d)' %(username_insert, password_insert, firstname_insert, lastname_insert, height_insert, weight, drinks_insert, dietaryRestrictions_insert, food_insert, amountOwed)
+    columnsStatement = 'insert into users (Height, Weight, Drinks, DietaryRestrictions, Food, AmountOwed)'
+    sql = columnsStatement + 'values(%s, %d, %s, %s, %s, %d)' %(height_insert, weight, drinks_insert, dietaryRestrictions_insert, food_insert, amountOwed)
     cursor.execute(sql)
     db.commit()
-addUserToDatabase('jbutler', 'frontend', 'Jacob', 'Butler', '5 feet 11 inches', 190, 'Ginger Ale', 'Not Applicable', 'Sandwiches', 0)
-print (checkUserName("amuharemi"))
+
+if __name__ == '__main__':
+    app.run(debug = True)
