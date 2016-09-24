e66495e9260e2abb2e38b6af32d0ee399ed32473
Arber Muharemi <arber.muharemi@gatech.edu>
2016-09-24 06:43:57 -0400

    added user_auth method

--- userAuthentication.py
+++ userAuthentication.py
@@ -2,10 +2,28 @@ import MySQLdb
 
 host = "localhost"
 user = "root"
-password = ""
+password = "hackgt2016"
 database = "user_accounts"
 
 db = MySQLdb.connect(host, user, password, database)
 
 cursor = db.cursor()
+
+def user_auth(username, password):
+    query = "select UserName, Password \
+             from users \
+             where UserName = username"
+    is_valid_user = False
+    try:
+        cursor.execute(query)
+        results = cursor.fetchall()
+        user = results[0][0]
+        print user
+        password = results[0][1]
+        print password
+    except:
+        print "No user exists"
+    return is_valid_user
+
+print user_auth("user", "password")
 
