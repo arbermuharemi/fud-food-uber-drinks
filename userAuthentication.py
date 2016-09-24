5bc9358e9c02a39078f7d85c39ff2225dc52cc0b
Rishabh Mahajani <rmahajani3@gatech.edu>
2016-09-24 07:47:25 -0400

    resolved sql accessing issues

--- userAuthentication.py
+++ userAuthentication.py
@@ -13,14 +13,14 @@ def user_auth(username, password):
     is_valid_user = False
     query = "select UserName, Password \
              from users \
-             where UserName = username"
+             where UserName = " + username;
     try:
         cursor.execute(query)
         results = cursor.fetchall()
-        db_user = results[0][0]
+        db_user = results[0]
         print results
         print db_user
-        db_password = results[0][1]
+        db_password = results[0]
         print password
         if username == db_user and password == db_password:
             is_valid_user = True
@@ -29,5 +29,5 @@ def user_auth(username, password):
     except:
         print "Username or password was invalid"
     return is_valid_user
-print user_auth("bob", "passo")
+print user_auth("\'rmahajani\'", "\'hackgt2016\'")
 
