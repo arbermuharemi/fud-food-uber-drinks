9ee7c3140552e4a1107805c625994df8e8de6b9c
Rishabh Mahajani <rmahajani3@gatech.edu>
2016-09-24 10:34:36 -0400

    Created functionality for drinks

--- foodStore.py
+++ foodStore.py
@@ -10,7 +10,7 @@ db = MySQLdb.connect(host, server_user, server_password, database)
 cursor = db.cursor()
 
 
-def purchase_food(food_item, quantity):
+def purchaseFood(food_item, quantity):
     food_item_insert = '\'' + food_item + '\''
     sqlget = "select Quantity from food where FoodName = %s" %food_item_insert
     cursor.execute(sqlget)
@@ -28,7 +28,7 @@ def purchase_food(food_item, quantity):
     db.commit()
     return True
 
-def calorieCount(food_item, quantity):
+def foodcalorieCount(food_item, quantity):
     food_item_insert = '\'' + food_item + '\''
     sql = "select Calories from Food where FoodName = %s" % food_item_insert
     cursor.execute(sql)
@@ -36,10 +36,54 @@ def calorieCount(food_item, quantity):
     caloriesPerItem = int(results[0][0])
     return caloriesPerItem * quantity
 
-def amountDue(username, food_item, quantity):
+def foodamountDue(username, food_item, quantity):
     food_item_insert = '\'' + food_item + '\''
     username_insert = '\'' + username + '\''
     foodsql = "select Price from Food where FoodName = %s" % food_item_insert
+    cursor.execute(foodsql)
+    results = cursor.fetchall()
+    pricePerItem = float(results[0][0])
+    totalPrice = pricePerItem * quantity
+    usersqlget = "select AmountOwed from users where UserName = %s" % username_insert
+    cursor.execute(usersqlget)
+    results = cursor.fetchall()
+    print (results)
+    totalAmountOwed = totalPrice + float(results[0][0])
+    usersqlpost = "update users set AmountOwed = %f where UserName = %s" % (totalAmountOwed, username_insert)
+    cursor.execute(usersqlpost)
+    db.commit()
+    return totalPrice
+
+def purchaseDrink(drink_item, quantity):
+    drink_item_insert = '\'' + drink_item + '\''
+    sqlget = "select Quantity from drink where DrinkName = %s" %drink_item_insert
+    cursor.execute(sqlget)
+    results = cursor.fetchall()
+    db_quantity = int(results[0][0])
+    try:
+        if (quantity > db_quantity):
+            raise Exception
+    except:
+        print ("You ordered more than what was available")
+        return False
+    new_quantity = db_quantity - quantity
+    sqlpost = "update drink set Quantity = %d where DrinkName = %s" % (new_quantity, drink_item_insert)
+    cursor.execute(sqlpost)
+    db.commit()
+    return True
+
+def drinkcalorieCount(drink_item, quantity):
+    drink_item_insert = '\'' + drink_item + '\''
+    sql = "select Calories from drink where DrinkName = %s" % drink_item_insert
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    caloriesPerItem = int(results[0][0])
+    return caloriesPerItem * quantity
+
+def drinkamountDue(username, drink_item, quantity):
+    drink_item_insert = '\'' + drink_item + '\''
+    username_insert = '\'' + username + '\''
+    foodsql = "select Price from drink where DrinkName = %s" % drink_item_insert
     cursor.execute(foodsql)
     results = cursor.fetchall()
     pricePerItem = float(results[0][0])
