5a9b549101056c39810675e9632729d5b0ce04ea
Rishabh Mahajani <rmahajani3@gatech.edu>
2016-09-25 01:33:32 -0400

    Fix merge problems

--- python/userAuthentication.py
+++ python/userAuthentication.py
@@ -11,6 +11,8 @@ db = MySQLdb.connect(host, server_user, server_password, database)
 
 cursor = db.cursor()
 currentUser = ""
+flightCode = ""
+dateOfDeparture = ""
 
 @app.route('/authenticate', methods = ['GET', 'POST'])
 def user_auth():
@@ -41,11 +43,72 @@ def user_auth():
     except:
         return redirect(url_for('incorrectLogin'))
     currentUser = original_user
-    return render_template('Application.html')
+    return render_template('FlightNumber.html')
 
 @app.route('/incorrectLogin')
 def incorrectLogin():
     return 'Your Login information was incorrect. Please try again'
+
+@app.route('/checkUserName', methods=['GET', 'POST'])
+def checkUserName():
+    sql = """select UserName from users;"""
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    isValid = True
+    user = request.form['userField']
+    myPass = request.form['passField']
+    first = request.form['firstName']
+    last = request.form['lastName']
+    for row in results:
+        for entry in row:
+            if (user == entry):
+                isValid = False
+    if (isValid):
+        return redirect(url_for('addUserToDatabase1', username = user, password = myPass, firstname = first, lastname = last))
+    else:
+        return redirect(url_for('incorrectUser'))
+
+@app.route('/incorrectUser')
+def incorrectUser():
+    return "This username is taken. Please sign up with another username."
+
+@app.route('/addUserToDatabase1/<username>/<password>/<firstname>/<lastname>')
+def addUserToDatabase1(username, password, firstname, lastname):
+    username_insert = '\'' + username + '\''
+    password_insert = '\'' + password + '\''
+    firstname_insert = '\'' + firstname + '\''
+    lastname_insert = '\'' + lastname + '\''
+    columnsStatement = 'insert into users (UserName, Password, FirstName, LastName)'
+    sql = columnsStatement + 'values(%s, %s, %s, %s)' %(username_insert, password_insert, firstname_insert, lastname_insert)
+    cursor.execute(sql)
+    db.commit()
+    return render_template('UserReg2.html')
+
+@app.route('/addUserToDatabase2', methods = ['GET', 'POST'])
+def addUserToDatabase2():
+    height = request.form['heightField']
+    weight = int(request.form['weightField'])
+    drinks = request.form['drinkSelect']
+    dietaryRestrictions = request.form.getlist('restrictionSelect');
+    dietaryRestrictions = ','.join(dietaryRestrictions)
+    print (dietaryRestrictions)
+    food = request.form['foodSelect']
+    amountOwed = 0;
+    drinks_insert = '\'' + drinks + '\''
+    dietaryRestrictions_insert = '\'' + dietaryRestrictions + '\''
+    food_insert = '\'' + food + '\''
+    height_insert = '\'' + height + '\''
+    columnsStatement = 'insert into users (Height, Weight, Drinks, DietaryRestrictions, Food, AmountOwed)'
+    sql = columnsStatement + 'values(%s, %d, %s, %s, %s, %d)' %(height_insert, weight, drinks_insert, dietaryRestrictions_insert, food_insert, amountOwed)
+    cursor.execute(sql)
+    db.commit()
+    return render_template('FlightNumber.html')
+
+@app.route('/storeFlightInfo', methods = ['GET', 'POST'])
+def storeFlightInfo():
+    flightCode = request.form['flightNumber']
+    dateOfDeparture = request.form['dateNumber']
+    return render_template('Application.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
